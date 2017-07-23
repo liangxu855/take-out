29b268474d7fc91ac7b72856c9b1d73e772db244
ZNB <1034741929@qq.com>
2017-07-23 14:53:58 +0800

    增加收货地址 和定位activity

---
+++ app/src/main/java/com/hasee/bh_takeout/ui/activity/ReceiptAddress.java
@@ -0,0 +1,18 @@
+package com.hasee.bh_takeout.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hasee.bh_takeout.R;
+
+/**
+ * Created by Administrator on 2017/7/23 0023.
+ */
+
+public class ReceiptAddress extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_receipt_address);
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/hasee/bh_takeout/ui/activity/SelectLocationActivity.java
@@ -0,0 +1,20 @@
+package com.hasee.bh_takeout.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hasee.bh_takeout.R;
+
+/**
+ * Created by Administrator on 2017/7/23 0023.
+ */
+
+public class SelectLocationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_select_location);
+        super.onCreate(savedInstanceState);
+    }
+
+}
