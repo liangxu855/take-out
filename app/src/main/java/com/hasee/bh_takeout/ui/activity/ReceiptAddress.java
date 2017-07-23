0e85cd8d54f7673ca6222483828e09aa87ad2d4d
ZNB <1034741929@qq.com>
2017-07-23 15:06:22 +0800

    butterknife

--- app/src/main/java/com/hasee/bh_takeout/ui/activity/ReceiptAddress.java
+++ app/src/main/java/com/hasee/bh_takeout/ui/activity/ReceiptAddress.java
@@ -2,17 +2,33 @@ package com.hasee.bh_takeout.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.hasee.bh_takeout.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Administrator on 2017/7/23 0023.
  */
 
-public class ReceiptAddress extends BaseActivity{
+public class ReceiptAddress extends BaseActivity {
+    @InjectView(R.id.ib_back)
+    ImageButton ibBack;
+    @InjectView(R.id.tv_title)
+    TextView tvTitle;
+    @InjectView(R.id.rv_receipt_address)
+    RecyclerView rvReceiptAddress;
+    @InjectView(R.id.tv_add_address)
+    TextView tvAddAddress;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_receipt_address);
+        ButterKnife.inject(this);
         super.onCreate(savedInstanceState);
     }
 }
