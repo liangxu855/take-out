463c4f14a16361bb862a3e8d2f2d47e948eccc94
ZNB <1034741929@qq.com>
2017-07-24 20:06:31 +0800

    添加收货地址

--- app/src/main/java/com/hasee/bh_takeout/AddressManage/ui/NewAddressActivity.java
+++ app/src/main/java/com/hasee/bh_takeout/AddressManage/ui/NewAddressActivity.java
@@ -2,6 +2,8 @@ package com.hasee.bh_takeout.AddressManage.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -9,8 +11,10 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hasee.bh_takeout.R;
+import com.hasee.bh_takeout.model.dao.bean.AddressBean;
 import com.hasee.bh_takeout.ui.activity.BaseActivity;
 
 import butterknife.ButterKnife;
@@ -20,7 +24,7 @@ import butterknife.InjectView;
  * Created by Administrator on 2017/7/24 0024.
  */
 
-class NewAddressActivity extends BaseActivity {
+public class NewAddressActivity extends BaseActivity {
     @InjectView(R.id.ib_back)
     ImageButton ibBack;
     @InjectView(R.id.tv_title)
@@ -57,6 +61,35 @@ class NewAddressActivity extends BaseActivity {
         setContentView(R.layout.activity_edit_receipt_address);
         ButterKnife.inject(this);
         super.onCreate(savedInstanceState);
-
+        btOk.setOnClickListener(onClickListener);
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String name = etName.getText().toString().trim();
+            String phoneNumber = etPhone.getText().toString().trim();
+            String receiptAddress = tvReceiptAddress.getText().toString().trim();
+            String detailAddress = etDetailAddress.getText().toString().trim();
+            int checkedRadioButtonId = rgSex.getCheckedRadioButtonId();
+            if(TextUtils.isEmpty(name)){
+                Toast.makeText(NewAddressActivity.this, "请输入姓名", Toast.LENGTH_SHORT).show();
+            }
+            if(TextUtils.isEmpty(phoneNumber)){
+                Toast.makeText(NewAddressActivity.this, "请输入手机号", Toast.LENGTH_SHORT).show();
+            }
+            if(TextUtils.isEmpty(receiptAddress)||TextUtils.isEmpty(detailAddress)){
+                Toast.makeText(NewAddressActivity.this, "请输入地址", Toast.LENGTH_SHORT).show();
+            }
+            AddressBean addressBean = new AddressBean(name,null,phoneNumber,receiptAddress,detailAddress,null,0,0);
+            //AddressDao addressDao = new AddressDao(MyApplication.getContext());
+            //addressDao.addAddress(addressBean);
+        }
+    };
 }
