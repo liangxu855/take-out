8547e2e5e656116548e2001e3aabce574f6cca74
liangxu855 <594291723@qq.com>
2017-07-24 11:49:55 +0800

    修正一些login中控件的名称

--- app/src/main/java/com/hasee/bh_takeout/ui/activity/LoginActivity.java
+++ app/src/main/java/com/hasee/bh_takeout/ui/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.hasee.bh_takeout.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Layout;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,14 +23,12 @@ import butterknife.OnClick;
 public class LoginActivity extends BaseActivity {
     @InjectView(R.id.iv_user_back)
     ImageView ivUserBack;
-    @InjectView(R.id.iv_user_password_login)
-    TextView ivUserPasswordLogin;
     @InjectView(R.id.tv_user_code)
     TextView tvUserCode;
     @InjectView(R.id.login)
     TextView login;
-    @InjectView(R.id.imageView)
-    ImageView imageView;
+    @InjectView(R.id.tv_user_login_type_change)
+    TextView tvUserLoginTypeChange;
 
     /*------------ 短信登录 ------------*/
     @InjectView(R.id.ll_login_phone)
@@ -47,6 +46,11 @@ public class LoginActivity extends BaseActivity {
     @InjectView(R.id.ll_login_password)
     LinearLayout llLoginPassword;
 
+    /*------------ 密码登陆：1；短信验证：2 ------------*/
+    public static int LOGINUSEPASSWORD = 1;
+    public static int LOGINUSEPHONE = 2;
+    public int loginType = LOGINUSEPHONE;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,19 +59,40 @@ public class LoginActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.iv_user_back, R.id.iv_user_password_login, R.id.tv_user_code, R.id.login})
+    @OnClick({R.id.iv_user_back, R.id.tv_user_login_type_change, R.id.tv_user_code, R.id.login})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.iv_user_back:
-
+            case R.id.tv_user_login_type_change:
+                changeLoginType();
                 break;
-            case R.id.iv_user_password_login:
-
+            case R.id.iv_user_back:
+                finish();
                 break;
             case R.id.tv_user_code:
                 break;
             case R.id.login:
+                onLogin();
                 break;
+        }
+    }
+
+    private void onLogin() {
+
+    }
+
+
+    private void changeLoginType() {
+        if (loginType == LOGINUSEPHONE) {
+            loginType = LOGINUSEPASSWORD;
+            tvUserLoginTypeChange.setText("短信登录");
+            llLoginPhone.setVisibility(View.GONE);
+            llLoginPassword.setVisibility(View.VISIBLE);
+
+        } else if (loginType == LOGINUSEPASSWORD) {
+            loginType = LOGINUSEPHONE;
+            tvUserLoginTypeChange.setText("密码登录");
+            llLoginPhone.setVisibility(View.VISIBLE);
+            llLoginPassword.setVisibility(View.GONE);
         }
     }
 }
