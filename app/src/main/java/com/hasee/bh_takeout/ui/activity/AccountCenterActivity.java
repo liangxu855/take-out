4f15d4fe1b3100899863f41781979b4809d0e786
laidou <564379170@qq.com>
2017-07-27 10:53:09 +0800

    结算中心的框架搭建,AcountCenterActivity为这个界面的Activity

---
+++ app/src/main/java/com/hasee/bh_takeout/ui/activity/AccountCenterActivity.java
@@ -0,0 +1,18 @@
+package com.hasee.bh_takeout.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hasee.bh_takeout.R;
+
+/**
+ * Created by laidoudou on 2017/7/26.
+ */
+
+public class AccountCenterActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_center_one);
+    }
+}
