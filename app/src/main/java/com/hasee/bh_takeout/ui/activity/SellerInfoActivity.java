fe649605c4d3518ccb75b419b98946d90b925275
laidou <564379170@qq.com>
2017-07-28 16:13:34 +0800

    添加了部分界面的代码

--- app/src/main/java/com/hasee/bh_takeout/ui/activity/SellerInfoActivity.java
+++ app/src/main/java/com/hasee/bh_takeout/ui/activity/SellerInfoActivity.java
@@ -1,11 +1,19 @@
 package com.hasee.bh_takeout.ui.activity;
 
 import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hasee.bh_takeout.R;
 
 /**
  * Created by dell on 2017/7/27.
  */
 
-public class SellerInfoActivity  extends Application{
-
+public class SellerInfoActivity  extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_detail);
+    }
 }
